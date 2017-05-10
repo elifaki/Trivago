e00a4b641d2e307ee2fa2262217bb07eaf07312f
elifaki <aleaf61@gmail.com>
2017-05-10 21:49:16 +0300

    Sign Up

---
+++ ConsoleApplication1/Nunit.cs
@@ -0,0 +1,44 @@
+﻿
+using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.IE;
+
+namespace MyTest
+{
+	class Nunit
+    {
+        IWebDriver webDriver;
+        IWebElement eMail;
+        IWebElement passWord;
+        IWebElement showPassWord;
+        IWebElement logIn;
+
+        [SetUp]
+        public void InitializeBrowser()
+        {
+            webDriver = new InternetExplorerDriver(@"C:\Users\e_talay\Downloads\IEDriverServer_beta_Win32_3.3.99.4");
+        }
+        [Test]
+        public void TestNunit()
+        {
+            webDriver.Navigate().GoToUrl("https://accounts.trivago.com/login#/sign-up");
+            eMail = webDriver.FindElement(By.Id("emailRegister"));
+            eMail.Clear();
+            eMail.SendKeys("example" + Keys.Alt + "q" + "example.com");
+            showPassWord = webDriver.FindElement(By.Id("register-show-pass"));
+            showPassWord.Click();
+            passWord = webDriver.FindElement(By.Id("register-pass"));
+            passWord.SendKeys("1q2w3e4r5T");
+            logIn = webDriver.FindElement(By.CssSelector("button[class='btn btn--primary btn--submit login-btn--submit mb-gutter fl-trailing js_tlgSignupSubmit']"));
+            logIn.Click();
+        }
+
+        [TearDown]
+        public void CloseBrowser()
+        {
+            webDriver.Close();
+        }
+
+      
+    }
+}
---
+++ ConsoleApplication1/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using OpenQA.Selenium;
+using OpenQA.Selenium.IE;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            IWebDriver webDriver = new InternetExplorerDriver();
+            webDriver.Navigate().GoToUrl("info.thy.com");
+        }
+    }
+}
